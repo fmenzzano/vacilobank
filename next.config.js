26b37fdb198df0da8ce68bb570b7b6deb7dcbc73
fmenzzano <fmenzzano@gmail.com>
2025-04-05 19:58:47 -0300

    fix: atualizar configuração do Next.js e PostCSS

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverActions: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
   eslint: {
     ignoreDuringBuilds: true
   }
